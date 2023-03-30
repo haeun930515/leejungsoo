a314f04e9095389119a503059eb2c127ea29469d
Haeun <haeun930515@gmail.com>
2023-03-31 02:45:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# leejungsoo
+
+A new Flutter project.
+# leejungsoo
---
+++ android/app/src/main/kotlin/com/example/leejungsoo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.leejungsoo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
